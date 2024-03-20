9be7b5dc520c19aaef3db2dd49df4ff06cdf9cca
hackmo <hackmo872@gmail.com>
2024-03-20 17:45:27 +0300

    meta title handling was added

--- pages/TestPage.vue
+++ pages/TestPage.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+useHead({
+  title: "test meta title"
+})
 definePageMeta({
   layout: 'custom' // It is used for changing layout of the component
 })
--- pages/[...error].vue
+++ pages/[...error].vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-
+useHead({
+  title: "404"
+})
 </script>
 
 <template>
